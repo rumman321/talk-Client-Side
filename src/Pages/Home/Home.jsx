23d5ff764f957cceb006c3a779d1b088ef4fb03c
rumman321 <cset-210652@dti.ac>
2025-02-19 18:41:26 +0600

    shadcn setup complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
           onChange={handleSearch} // Trigger handleSearch on every change
         />
       </div>
-
+      
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-6">Recent Posts</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-6">
